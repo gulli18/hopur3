f0d4b46d967be44e303abbf594bcb1c403776492
Gunnlaugur Birgisson <ghb@Gunnlaugurs-MacBook-Pro.local>
2018-05-08 16:07:01 +0000

    popup container added and 80% complete

--- BookCave/wwwroot/js/site.js
+++ BookCave/wwwroot/js/site.js
@@ -1,1 +1,18 @@
 ﻿// Write your JavaScript code.
+$( document ).ready(function() {
+
+    $("#bottom-nav-books").mouseover(function() {
+        $("#popup-container").toggle();
+        $("#popup-container h4 a").text("Books");
+    });
+
+    $("#popup-container").mouseleave(function() {
+        $("#popup-container").toggle();
+    });
+
+    $("#bottom-nav-audio").mouseover(function() {
+        $("#popup-container").toggle();
+        $("#popup-container h4 a").text("Audiobooks");
+    })
+
+});
