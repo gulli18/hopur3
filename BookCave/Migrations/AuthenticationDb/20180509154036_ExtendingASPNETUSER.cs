5be031c400c5734c21eb7cc0f225dab4f7e06ca8
solvii <solviv16@ru.is>
2018-05-09 22:35:53 +0000

    database update

---
+++ BookCave/Migrations/AuthenticationDb/20180509154036_ExtendingASPNETUSER.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace BookCave.Migrations.AuthenticationDb
+{
+    public partial class ExtendingASPNETUSER : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Age",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "BillingAdressId",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "CardInformationId",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "FavoriteBook",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Image",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "OrderListId",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ShippingAdressId",
+                table: "AspNetUsers",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Age",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "BillingAdressId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "CardInformationId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "FavoriteBook",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Image",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "OrderListId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "ShippingAdressId",
+                table: "AspNetUsers");
+        }
+    }
+}
---
+++ BookCave/Models/ViewModels/ProfileViewModel.cs
@@ -0,0 +1,12 @@
+namespace BookCave.Models.ViewModels
+{
+  public class ProfileViewModel
+  {
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string FavoriteBook { get; set; }
+    public string Age { get; set; }
+    public string Image { get; set; }
+
+  }
+}
