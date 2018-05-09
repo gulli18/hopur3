93e99d888ff344e143b82395525665a23c1a6f0e
Gunnlaugur Birgisson <ghb@Gunnlaugurs-MacBook-Pro.local>
2018-05-09 18:39:19 +0000

    css author index updated

--- BookCave/wwwroot/js/site.js
+++ BookCave/wwwroot/js/site.js
@@ -1,8 +1,17 @@
 ﻿// Write your JavaScript code.
+$(document).ready(function(){
 
-$("#winner-button").click(function() {
-  $.get("GetWinner", function(data, status){
-    console.log(data);
-    $("#winner-book").append(data.title);
+  $("#winner-button").click(function() {
+    $.get("GetWinner", function(data, status){
+      console.log(data);
+      $("#winner-book").append(data.title);
+    });
+  });
+
+  $("#authorsInput").on("keyup", function() {
+    var value = $(this).val().toLowerCase();
+    $("#authorsTable tr").filter(function() {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+    });
   });
 });
