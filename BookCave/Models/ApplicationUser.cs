178be3bbc919feee195366a2f9c6c13128b606f3
brynjargauti <brynjarthorsteins@gmail.com>
2018-05-09 14:43:34 +0000

    applicationuser

--- BookCave/Models/ApplicationUser.cs
+++ BookCave/Models/ApplicationUser.cs
@@ -1,9 +1,19 @@
+using System.Collections.Generic;
+using BookCave.Data.EntityModels;
 using Microsoft.AspNetCore.Identity;
 
 namespace BookCave.Models
 {
   public class ApplicationUser : IdentityUser
   {
-    
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string FavoriteBook { get; set; }
+    public string Age { get; set; }
+    public string Image { get; set; }
+    public string BillingAdressId { get; set; }
+    public string ShippingAdressId { get; set; }
+    public string CardInformationId { get; set; }
+    public string OrderListId { get; set; }
   }
 }
