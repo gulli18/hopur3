6ff3d5981c15882f451baa822ab85cb896733983
Hafsteinn <hafsteinn24@gmail.com>
2018-05-09 19:49:54 +0000

    choose for me ordid lit

--- BookCave/wwwroot/js/site.js
+++ BookCave/wwwroot/js/site.js
@@ -1,8 +1,23 @@
 ﻿// Write your JavaScript code.
 
+var buttonclick = 0;
 $("#winner-button").click(function() {
   $.get("GetWinner", function(data, status){
-    console.log(data);
-    $("#winner-book").append(data.title);
+    buttonclick++;
+
+    if(buttonclick > 0) 
+    {
+      $("#winner-title").text('');
+      $("#winner-author").text('');
+      $("#winner-rating").text('');
+      $("#winner-price").text('');
+      $("#winner-format").text('');
+    }
+    $("#winner-title").append(data.title);
+    $("#winner-author").append(data.author);
+    $("#winner-rating").append(data.rating);
+    $("#winner-price").append(data.price);
+    $("#winner-format").append(data.format);
+    $("#winner-image").attr("src", data.image);
   });
 });
