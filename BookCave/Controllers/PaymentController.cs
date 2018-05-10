2acf943f2140e49a6719ffebecf8d7820aa43561
brynjargauti <brynjarthorsteins@gmail.com>
2018-05-10 22:51:42 +0000

    payment controller

--- BookCave/Controllers/PaymentController.cs
+++ BookCave/Controllers/PaymentController.cs
@@ -6,21 +6,35 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using BookCave.Models;
 using BookCave.Services;
+using Microsoft.AspNetCore.Identity;
+using BookCave.Models.ViewModels;
+using Microsoft.AspNetCore.Authorization;
 
 namespace BookCave.Controllers
 {
     public class PaymentController : Controller
     {
-       
-        
-        public PaymentController()
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public PaymentController(SignInManager<ApplicationUser> signInManager, UserManager<ApplicationUser> userManager)
         {
-            
+            _signInManager = signInManager;
+            _userManager = userManager;
         }
 
-        public IActionResult Delivery()
+        [Authorize]
+        public async Task<IActionResult> Delivery()
         {
-            return View();
+            var user = await _userManager.GetUserAsync(User);
+
+            return View(new BillingAddressViewModel
+            {
+                StreetAdress = user.BillingStreetAdress,
+                TownCity = user.BillingTownCity,
+                ZipPostcode = user.BillingZipPostcode,
+                Country = user.BillingCountry
+            });
         }
 
         public IActionResult Pay()
